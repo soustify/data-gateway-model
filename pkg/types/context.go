0631b432bc82e1108480f9356d46f85556b65fd2
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-10-11 17:24:35 -0300

    change base

--- pkg/types/context.go
+++ pkg/types/context.go
@@ -21,6 +21,14 @@ type (
 	}
 )
 
+func (c *LambdaParameter[any]) GenerateContext() context.Context {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, ContextId, c.Metadata.ContextId)
+	ctx = context.WithValue(ctx, CognitoPoolId, c.Metadata.ContextId)
+	ctx = context.WithValue(ctx, CognitoUserId, c.Metadata.ContextId)
+	return ctx
+}
+
 func CreateContext(value MetadataContext) context.Context {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, ContextId, value.ContextId)
