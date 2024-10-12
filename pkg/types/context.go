aed295fbc8208c3c3e7e90b02b1fb172bb422e4c
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-10-12 00:54:47 -0300

    validations

--- pkg/types/context.go
+++ pkg/types/context.go
@@ -1,6 +1,10 @@
 package types
 
-import "context"
+import (
+	"context"
+	"errors"
+	"fmt"
+)
 
 const (
 	ContextId     = "context-id"
@@ -12,6 +16,7 @@ type (
 	LambdaParameter[T any] struct {
 		Content  *T
 		Metadata *MetadataContext
+		fields   []string
 	}
 
 	MetadataContext struct {
@@ -21,9 +26,66 @@ type (
 	}
 )
 
-func (c *LambdaParameter[any]) GenerateContext(ctx context.Context) context.Context {
+func FullValidation(meta *MetadataContext) error {
+	if meta == nil {
+		return errors.New("metada is null")
+	}
+	if meta.ContextId == "" || meta.CognitoPoolId == "" || meta.CognitoUserId == "" {
+		return errors.New(fmt.Sprintf("metadata fields invalid, Context: %s, PollId: %s UserId: %s", meta.ContextId, meta.CognitoPoolId, meta.CognitoUserId))
+	}
+	return nil
+}
+
+func OnlyContextValidation(meta *MetadataContext) error {
+	if meta == nil {
+		return errors.New("metada is null")
+	}
+	if meta.ContextId == "" {
+		return errors.New(fmt.Sprintf("metadata fields invalid, Context: %s, PollId: %s UserId: %s", meta.ContextId, meta.CognitoPoolId, meta.CognitoUserId))
+	}
+	return nil
+}
+
+func ContextAndUserIdValidation(meta *MetadataContext) error {
+	if meta == nil {
+		return errors.New("metada is null")
+	}
+	if meta.ContextId == "" || meta.CognitoUserId == "" {
+		return errors.New(fmt.Sprintf("metadata fields invalid, Context: %s, UserId: %s", meta.ContextId, meta.CognitoUserId))
+	}
+	return nil
+}
+
+func ContextAndPoolIdValidation(meta *MetadataContext) error {
+	if meta == nil {
+		return errors.New("metada is null")
+	}
+	if meta.ContextId == "" || meta.CognitoPoolId == "" {
+		return errors.New(fmt.Sprintf("metadata fields invalid, Context: %s, PollId: %s", meta.ContextId, meta.CognitoPoolId))
+	}
+	return nil
+}
+
+func UserIdAndPoolIdValidation(meta *MetadataContext) error {
+	if meta == nil {
+		return errors.New("metada is null")
+	}
+	if meta.CognitoPoolId == "" || meta.CognitoUserId == "" {
+		return errors.New(fmt.Sprintf("metadata fields invalid, PollId: %s UserId: %s", meta.CognitoPoolId, meta.CognitoUserId))
+	}
+	return nil
+}
+
+func (c *LambdaParameter[any]) GenerateContext(ctx context.Context, validations ...func(metadataContext *MetadataContext) error) (context.Context, error) {
+	if validations != nil {
+		for _, validation := range validations {
+			if err := validation(c.Metadata); err != nil {
+				return nil, err
+			}
+		}
+	}
 	ctx = context.WithValue(ctx, ContextId, c.Metadata.ContextId)
 	ctx = context.WithValue(ctx, CognitoPoolId, c.Metadata.CognitoPoolId)
 	ctx = context.WithValue(ctx, CognitoUserId, c.Metadata.CognitoUserId)
-	return ctx
+	return ctx, nil
 }
